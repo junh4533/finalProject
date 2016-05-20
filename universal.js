b7db31c16eb2df76364f17f3281ff2f2459b8d65
junh4533 <junh4533@hstat.org>
2016-05-20 06:00:41 +0000

    add universal.js

---
+++ universal.js
@@ -0,0 +1,61 @@
+
+$(document).ready(function() {
+
+//slide animations
+    $(window).scroll(function() {
+
+        //animation1
+        $(".slideanim").each(function() {
+
+            var pos = $(this).offset().top;
+
+            var winTop = $(window).scrollTop();
+            if (pos < winTop + 500) {
+                $(this).addClass("slideUp");
+            }
+
+        });
+
+        //animation2
+        $(".slideanim2").each(function() {
+
+            var pos2 = $(this).offset().top;
+
+            var winTop = $(window).scrollTop();
+            if (pos2 < winTop + 500) {
+                $(this).addClass("slideLeft");
+            }
+
+        });
+
+        //animation3
+        $(".slideanim3").each(function() {
+
+            var pos2 = $(this).offset().top;
+
+            var winTop = $(window).scrollTop();
+            if (pos2 < winTop + 500) {
+                $(this).addClass("slideRight");
+            }
+
+        });
+
+
+        //window scroll function end
+    });
+
+    //smooth scrolling
+    $('a[href^="#"]').on('click', function(e) {
+        e.preventDefault();
+        var target = this.hash; // sets var target to the link
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 1000, 'swing', function() {
+            window.location.hash = target; //scrolls to the targeted section
+        });
+    });
+
+
+});
